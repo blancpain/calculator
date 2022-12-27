7c33ffcf5c660ce8cfa803e89f20400766d31712
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-28 01:04:57 +0200

    Add decimal button functionality

--- script.js
+++ script.js
@@ -26,10 +26,12 @@ function operate(operator, a, b) {
 const display = document.querySelector("#current_selection");
 const results = document.querySelector("#results");
 const buttons = document.querySelectorAll("button");
+const decimalButton = document.getElementById("decimal");
 
 let userInput = "";
 let confirmedNumA;
 let confirmedNumB;
+let finalResult;
 let operator;
 
 buttons.forEach((button) =>
@@ -38,11 +40,12 @@ buttons.forEach((button) =>
       userInput += button.textContent;
       display.textContent = userInput;
     } else if (button.className === "operator") {
-      results.textContent = userInput + " " + button.textContent;
       operator = button.textContent;
       if (typeof confirmedNumA === "undefined") {
+        results.textContent = userInput + " " + button.textContent;
         confirmedNumA = Number(userInput);
       } else {
+        results.textContent = `${confirmedNumA} ${operator}`;
         confirmedNumB = Number(userInput);
       }
       userInput = "";
@@ -52,18 +55,46 @@ buttons.forEach((button) =>
       results.textContent = "";
       confirmedNumA = undefined;
       confirmedNumB = undefined;
+      operator = undefined;
     } else if (button.id === "equals") {
       confirmedNumB = Number(userInput);
       if (
         typeof confirmedNumA !== "undefined" &&
         typeof confirmedNumB !== "undefined"
       ) {
-        let finalResult = operate(operator, confirmedNumA, confirmedNumB);
-        results.textContent = `${confirmedNumA} ${operator} ${confirmedNumB}  =`;
+        finalResult = operate(operator, confirmedNumA, confirmedNumB);
+        results.textContent = `${confirmedNumA} ${operator} ${confirmedNumB} =`;
         display.textContent = finalResult;
         confirmedNumA = finalResult;
-        confirmedNumB = undefined;
+        confirmedNumB = 0;
+      }
+    } else if (button.id == "decimal") {
+      if (confirmedNumB !== 0 || confirmedNumB !== undefined) {
+        confirmedNumB = 0;
+      }
+      if (!String(confirmedNumA).includes(".") && !userInput.includes(".")) {
+        userInput += button.textContent;
+        display.textContent += button.textContent;
+      } else if (
+        !String(confirmedNumB).includes(".") &&
+        !userInput.includes(".")
+      ) {
+        userInput += button.textContent;
+        display.textContent += button.textContent;
       }
     }
+    // else if (button.id === "percentage") {
+    //   confirmedNumA = Number(userInput) / 100;
+    //   display.textContent = confirmedNumA;
+    // }
+    // else if (button.id === "toggle_sign") {
+    //   if (typeof confirmedNumA === "undefined") {
+    //     confirmedNumA = -Number(userInput);
+    //     display.textContent = confirmedNumA;
+    //   } else {
+    //     confirmedNumB = -Number(userInput);
+    //     display.textContent = confirmedNumB;
+    //   }
+    // }
   })
 );
