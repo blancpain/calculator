c2937730dc85c21b9de94dac1fc7ff09f2913bf7
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-27 23:04:53 +0200

    Add event listener for buttons and basic calc functionality

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
+
+const display = document.querySelector("#current_selection");
+const results = document.querySelector("#results");
+const buttons = document.querySelectorAll("button");
+
+let userInput = "";
+let confirmedNumA;
+let confirmedNumB;
+let operator;
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (button.className === "number") {
+      userInput += button.textContent;
+      display.textContent = userInput;
+    } else if (button.className === "operator") {
+      results.textContent = userInput + " " + button.textContent;
+      operator = button.textContent;
+      if (typeof confirmedNumA === "undefined") {
+        confirmedNumA = Number(userInput);
+      } else {
+        confirmedNumB = Number(userInput);
+      }
+      userInput = "";
+    } else if (button.className === "C") {
+      userInput = "";
+      display.textContent = "0";
+      results.textContent = "";
+      confirmedNumA = undefined;
+      confirmedNumB = undefined;
+    } else if (button.id === "equals") {
+      confirmedNumB = Number(userInput);
+      if (
+        typeof confirmedNumA !== "undefined" &&
+        typeof confirmedNumB !== "undefined"
+      ) {
+        let finalResult = operate(operator, confirmedNumA, confirmedNumB);
+        results.textContent = `${confirmedNumA} ${operator} ${confirmedNumB}  =`;
+        display.textContent = finalResult;
+        confirmedNumA = finalResult;
+        confirmedNumB = undefined;
+      }
+    }
+  })
+);
