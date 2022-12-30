6e9dd2fa936d78316813dea595d3312c90540e13
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-30 16:12:41 +0200

    Fix issue with decimal numbers causing leading to incorrect calcs

--- script.js
+++ script.js
@@ -22,31 +22,29 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
+
 //TODO: create an object for the different inputs - number, operator, equals, decimal etc and refer to the relevant functions
 
 function calculateWithEquals(a, b, operator) {
-  if (
-    a !== undefined &&
-    b !== undefined &&
-    textOfFinalOutcome !== results.textContent
-  ) {
-    finalResult = operate(operator, a, b);
+  if (a !== undefined && b !== undefined) {
+    currentTotal = operate(operator, a, b);
     results.textContent = `${a} ${operator} ${b} =`;
-    display.textContent = finalResult;
-    confirmedNumA = finalResult;
-    textOfFinalOutcome = results.textContent;
+    display.textContent = currentTotal;
+    confirmedNumA = currentTotal;
+    initialUserInput = currentTotal;
     confirmedNumB = undefined;
+    usedEquals = true;
   } else {
     return;
   }
 }
 
 function calculateWithOperator(selectedOperator, e) {
-  finalResult = operate(selectedOperator, confirmedNumA, confirmedNumB);
+  currentTotal = operate(selectedOperator, confirmedNumA, confirmedNumB);
   operator = e.target.textContent;
-  results.textContent = `${finalResult} ${operator}`;
-  display.textContent = finalResult;
-  confirmedNumA = finalResult;
+  results.textContent = `${currentTotal} ${operator}`;
+  display.textContent = currentTotal;
+  confirmedNumA = currentTotal;
   tempNumberB = confirmedNumB;
   confirmedNumB = undefined;
 }
@@ -54,15 +52,26 @@ function calculateWithOperator(selectedOperator, e) {
 function useOperator(selectedOperator, e) {
   //default to 0 if user hasn't entered "number one" and clicks an operator
   if (confirmedNumA === undefined) {
-    confirmedNumA = Number(userInput);
+    confirmedNumA = Number(initialUserInput);
   }
   //to be used in scenario 3 below
   timeStamp2 = e.timeStamp;
   results.textContent = confirmedNumA + " " + selectedOperator;
   if (confirmedNumB === undefined) {
-    userInput = "";
+    initialUserInput = "";
     return;
-  } else if (finalResult !== confirmedNumA) {
+  } else if (
+    String(confirmedNumB).includes(".") &&
+    usedEquals === true &&
+    !String(currentTotal).includes(".")
+  ) {
+    results.textContent = confirmedNumB + " " + selectedOperator;
+    confirmedNumA = confirmedNumB;
+    confirmedNumB = undefined;
+    initialUserInput = "";
+    usedEquals = false;
+    return;
+  } else if (currentTotal !== confirmedNumA) {
     //for first time calcs using an operator, we need to make sure there isn't a final result yet
     calculateWithOperator(selectedOperator, e);
   } else if (tempNumberB !== confirmedNumB) {
@@ -72,11 +81,11 @@ function useOperator(selectedOperator, e) {
     //scenario 3: to catch situations where user has changed "number 2" but it is the same number,
     calculateWithOperator(selectedOperator, e); //in that case we look at the timeStamp of the clicks to differentiate the numbers
   }
-  userInput = "";
+  initialUserInput = "";
 }
 
 function clear() {
-  userInput = "";
+  initialUserInput = "";
   display.textContent = "0";
   results.textContent = "";
   confirmedNumA = undefined;
@@ -90,21 +99,22 @@ const buttons = document.querySelectorAll("button");
 const decimalButton = document.getElementById("decimal");
 
 //variables
-let textOfFinalOutcome = "";
-let userInput = "";
+let initialUserInput = "";
+let tempNumberB;
 let confirmedNumA;
 let confirmedNumB;
-let tempNumberB;
-let finalResult;
+let currentTotal;
 let operator;
+let usedEquals = false;
 
 buttons.forEach((button) =>
   button.addEventListener("click", (e) => {
     if (button.className === "number") {
-      userInput += button.textContent;
-      display.textContent = userInput;
+      initialUserInput += button.textContent;
+      display.textContent = initialUserInput;
       if (confirmedNumA !== undefined) {
-        confirmedNumB = Number(userInput);
+        confirmedNumB = Number(initialUserInput);
+        //get timeStamp of initial number 2 assignment
         timeStamp1 = e.timeStamp;
       }
     } else if (button.className === "operator") {
@@ -116,28 +126,27 @@ buttons.forEach((button) =>
       clear();
     } else if (button.id === "equals") {
       calculateWithEquals(confirmedNumA, confirmedNumB, operator);
-    } else if (button.id == "decimal") {
-      if (!String(confirmedNumA).includes(".") && !userInput.includes(".")) {
-        userInput += button.textContent;
-        display.textContent += button.textContent;
-      } else if (
-        !String(confirmedNumB).includes(".") &&
-        !userInput.includes(".")
-      ) {
-        userInput += button.textContent;
+    }
+    // decimal operator functionality
+    else if (button.id == "decimal") {
+      if (!display.textContent.includes(".")) {
+        initialUserInput += button.textContent;
         display.textContent += button.textContent;
       }
     }
+    //BELOW functionality is OPTIONAL.....
+    //percentage - to be revised & included
     // else if (button.id === "percentage") {
-    //   confirmedNumA = Number(userInput) / 100;
-    //   display.textContent = confirmedNumA;
+    //   initialUserInput /= 100;
+    //   display.textContent = initialUserInput;
     // }
+    //toggle sign - to be revised & included
     // else if (button.id === "toggle_sign") {
     //   if (confirmedNumA === undefined) {
-    //     confirmedNumA = -Number(userInput);
+    //     confirmedNumA = -Number(initialUserInput);
     //     display.textContent = confirmedNumA;
     //   } else {
-    //     confirmedNumB = -Number(userInput);
+    //     confirmedNumB = -Number(initialUserInput);
     //     display.textContent = confirmedNumB;
     //   }
     // }
